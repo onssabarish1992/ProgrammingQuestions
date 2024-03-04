ec40a0400f110c5fb8d5cff30abb881709f4469e
Sabarish Nair <121101278@umail.ucc.ie>
2024-03-04 10:53:56 +0000

    ValidateParanthesis
    ValidateParanthesis

---
+++ ValidateParanthesis/Program.cs
@@ -0,0 +1,33 @@
+﻿
+Console.WriteLine(IsValid("([])"));
+Console.ReadKey();
+
+bool IsValid(string s)
+{
+    Stack<char> stack = new Stack<char>();
+
+    foreach (var character in s)
+    {
+        if(character == '(' || character == '{' || character == '[')
+        {
+            stack.Push(character);
+        }
+        else if(character ==  ')' && (stack.Count == 0 || stack.Pop() != '('))
+        {
+            return false;
+        }
+        else if (character == '}' && (stack.Count == 0 || stack.Pop() != '{'))
+        {
+            return false;
+        }
+        else if (character == ']' && (stack.Count == 0 || stack.Pop() != '['))
+        {
+            return false;
+        }
+    }
+
+    return stack.Count == 0;
+}
+
+
+Console.ReadKey();
