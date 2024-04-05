5610d672fcb23a6239acdb8cae0b0b364a6dbcee
Sabarish Nair <121101278@umail.ucc.ie>
2024-04-05 15:12:06 +0100

    GoodPair
    Programming Question

---
+++ GoodPair/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+/*
+Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+Example 2:
+
+Input: nums = [1,1,1,1]
+Output: 6
+Explanation: Each pair in the array are good.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 0
+*/
+
+int[] arr = { 1, 2,3};
+
+int good_pair_count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+        if(arr[i] == arr[j])
+        {
+            good_pair_count++;
+        }
+    }
+}
+
+Console.WriteLine(good_pair_count);
+Console.ReadKey();
